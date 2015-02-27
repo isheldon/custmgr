85183433d98d4741153e6dc31d6e35b747fa6d30
Sheldon Xu <sheldon.xu@ymail.com>
2015-02-27 17:20:54 +0800

    change default pwd

--- db/data-mysql.sql
+++ db/data-mysql.sql
@@ -1,2 +1,2 @@
 insert  into `user`(`user_name`, `real_name`, `password`, `role`)
-values ('admin', '管理员', 'adminpwd', 'ADMIN');
+values ('admin', '管理员', 'cb8f03cb6ec7c55d021cd22e06e038d0', 'ADMIN');
