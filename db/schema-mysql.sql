9100a1455cf7699ddf7d71a81a0b992b18f2d42e
Sheldon Xu <sheldon.xu@ymail.com>
2015-02-27 16:40:12 +0800

    expand password field length

--- db/schema-mysql.sql
+++ db/schema-mysql.sql
@@ -2,7 +2,7 @@ CREATE TABLE `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(20) NOT NULL,
   `real_name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(20) NOT NULL,
+  `password` VARCHAR(80) NOT NULL,
   `manager_id` VARCHAR(45) NULL,
   `role` VARCHAR(30) NULL DEFAULT 'SALES',
   PRIMARY KEY (`id`),
